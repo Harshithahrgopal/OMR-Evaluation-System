7b7d3d9d9b6aa9253d00ef737d361ee8cfed7cb4
Dyuthiprasad <dyuthi.cs23@bmsce.ac.in>
2025-09-20 19:21:31 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "postgresql://user:password@localhost/omrdb"
+DATABASE_URL = "postgresql://postgres:OmrHackathon@db.yefpuwzyovxluoiltnrd.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
